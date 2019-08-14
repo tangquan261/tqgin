a7ed5e1724e5bee8a3b5fc400c1b34dcf8c3e201
tangquan <tangquan@xintiaotime.com>
2019-08-14 21:00:06 +0800

    db打日志。

--- models/Model.go
+++ models/Model.go
@@ -2,13 +2,18 @@ package models
 
 import (
 	"fmt"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
+	rotatelogs "github.com/lestrrat-go/file-rotatelogs"
+	"github.com/rifflock/lfshook"
+	"github.com/sirupsen/logrus"
 )
 
 var (
-	DB *gorm.DB
+	DB    *gorm.DB
+	DBLog *logrus.Logger
 )
 
 func init() {
@@ -27,6 +32,7 @@ func init() {
 	DB.DB().SetMaxIdleConns(10)
 	DB.DB().SetMaxOpenConns(100)
 	DB.LogMode(true)
+	DB.SetLogger(NewLogger())
 	DB.SingularTable(true)
 	DB.Set("gorm:table_options", "ENGINE=InnoDB").AutoMigrate(
 		&Account{}, &UserInfo{}, &RoomInfo{}, &RoomPowerMemberInfo{},
@@ -41,6 +47,34 @@ func init() {
 
 func loadConf() {
 	GetAllGift()
+}
+
+func NewLogger() *logrus.Logger {
+	if DBLog != nil {
+		return DBLog
+	}
+
+	path := "./dblog/dbinfo.log"
+
+	writer, _ := rotatelogs.New(
+		path+".%Y%m%d%H%M",
+		rotatelogs.WithLinkName(path),
+		rotatelogs.WithMaxAge(time.Duration(86400)*time.Second),        //文件最大保存时间
+		rotatelogs.WithRotationTime(time.Duration(604800)*time.Second), //日志切割时间间隔
+	)
+
+	DBLog = logrus.New()
+
+	DBLog.Hooks.Add(lfshook.NewHook(
+		lfshook.WriterMap{
+			logrus.InfoLevel:  writer,
+			logrus.ErrorLevel: writer,
+			logrus.DebugLevel: writer,
+		},
+		&logrus.JSONFormatter{},
+	))
+
+	return DBLog
 }
 
 /*
